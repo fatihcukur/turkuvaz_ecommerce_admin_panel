6e503413d5b99b355aa4e30c8160b95b6beb6300
Fatih Çukur <fatihcukur1@hotmail.com>
2026-08-26 09:05:50 +0300

    feat: product addition form added

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_title',
+        'product_category_id',
+        'barcode',
+        'product_status',
+    ];
+}
